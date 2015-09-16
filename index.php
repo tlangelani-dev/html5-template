a83505f873cd5b57d97ed5239f0eb87547a2775c
Tlangelani Mboweni <mbowenit@gmail.com>
2015-09-16 10:38:43 +0200

    add css, js, ims dir

--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>HTML5</title>
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
     
+    <div class="wrapper">
+        <h1>HTML5 Template</h1>
+    </div><!-- end .wrapper -->
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/script.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
